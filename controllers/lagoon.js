b71ae69b4ccb44d8c9f739f5da98a489c077a102
prakasit homklin <prakasit.apsofttech@gmail.com>
2014-05-27 17:10:36 +0700

    xml read array5

--- controllers/lagoon.js
+++ controllers/lagoon.js
@@ -50,7 +50,7 @@ res.end('No such file exist');
 
 
 );
-console.log(data.test_server.zipcodes[0].zipcode[0]);
+//console.log(data.test_server.zipcodes[0].zipcode[0]);
 //var conn = mongoose.connection();
 
 
