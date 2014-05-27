3899c0825c8590d3e8fc6e110b0dc4000d477021
prakasit homklin <prakasit.apsofttech@gmail.com>
2014-05-27 13:09:58 +0700

    new branch

--- app.js
+++ app.js
@@ -31,17 +31,7 @@ app.get('/', function (req, res) {
   res.sendfile('/index.html');
 });
 
-app.get( "/crossdomain.xml", onCrossDomainHandler )
-function onCrossDomainHandler( req, res ) {
-  var xml = '<?xml version="1.0"?>\n<!DOCTYPE cross-domain-policy SYSTEM' + 
-            ' "http://www.macromedia.com/xml/dtds/cross-domain-policy.dtd">\n<cross-domain-policy>\n';
-      xml += '<allow-access-from domain="*" to-ports="*"/>\n';
-      xml += '</cross-domain-policy>\n';
-  
-  req.setEncoding('utf8');
-  res.writeHead( 200, {'Content-Type': 'text/xml'} );
-  res.end( xml );  
-}
+
 
 app.get('/page',lagoon.getpage);
 // Restful API
